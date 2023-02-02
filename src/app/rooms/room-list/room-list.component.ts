23c7d1548d2b687ddd4387370cfad1fb95ad3ec6
Md. Mostafizur Rahman <mrahman.cse32@gmail.com>
2023-02-02 12:51:55 +0600

    OnChanges lifecycle hooks

--- src/app/rooms/room-list/room-list.component.ts
+++ src/app/rooms/room-list/room-list.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter,ChangeDetectionStrategy, OnChanges, SimpleChanges } from '@angular/core';
 import { RoomList } from '../rooms';
+// import { ChangeDetectionStrategy } from '@angular/compiler';
 
 @Component({
   selector: 'app-room-list',
   templateUrl: './room-list.component.html',
-  styleUrls: ['./room-list.component.scss']
+  styleUrls: ['./room-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.Default
 })
-export class RoomListComponent implements OnInit {
+export class RoomListComponent implements OnInit, OnChanges {
 
   @Input() rooms:RoomList[]=[];
+  @Input() title: string = '';
   @Output() selectedRoom = new EventEmitter<RoomList>();
+
+
   constructor(){}
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['title']){
+      this.title = changes['title'].currentValue.toUpperCase();
+    }
+  }
 
   ngOnInit(): void {
     throw new Error('Method not implemented.');
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -8,13 +8,13 @@ import { Room, RoomList } from './rooms';
 })
 export class RoomsComponent implements OnInit {
 
-  
+
 
   hotelName = "Hilton Hotel";
   numberOfRooms = 10;
   hideRooms = false;
 
-  selectedRoom! : RoomList;
+  selectedRoom!: RoomList;
 
   rooms: Room = {
     totalRooms: 20,
@@ -23,58 +23,75 @@ export class RoomsComponent implements OnInit {
   };
 
 
-  roomList:RoomList[] = [];
+  roomList: RoomList[] = [];
 
-  constructor(){}
+  constructor() { }
 
 
   ngOnInit(): void {
     this.roomList = [
       {
-        roomNumber:1,
+        roomNumber: 1,
         roomType: 'Delux Room',
         amenities: 'Air Conditioner, Free Wi-Fi, Tv, Bathroom, Kitchen',
-        price : 500,
-        photos:'',
+        price: 500,
+        photos: '',
         checkinTime: new Date('11-Nov-2021'),
         checkoutTime: new Date('12-Nov-2021')
       },
       {
-        roomNumber:2,
+        roomNumber: 2,
         roomType: 'Delux Room',
         amenities: 'Air Conditioner, Free Wi-Fi, Tv, Bathroom, Kitchen',
-        price : 500,
-        photos:'',
+        price: 500,
+        photos: '',
         checkinTime: new Date('11-Nov-2021'),
         checkoutTime: new Date('12-Nov-2021')
       },
       {
-        roomNumber:3,
+        roomNumber: 3,
         roomType: 'Delux Room',
         amenities: 'Air Conditioner, Free Wi-Fi, Tv, Bathroom, Kitchen',
-        price : 1200,
-        photos:'',
+        price: 1200,
+        photos: '',
         checkinTime: new Date('11-Nov-2021'),
         checkoutTime: new Date('12-Nov-2021')
       },
       {
-        roomNumber:4,
+        roomNumber: 4,
         roomType: 'Prive Suite',
         amenities: 'Air Conditioner, Free Wi-Fi, Tv, Bathroom, Kitchen',
-        price : 1500,
-        photos:'',
+        price: 1500,
+        photos: '',
         checkinTime: new Date('11-Nov-2021'),
         checkoutTime: new Date('12-Nov-2021')
       },
     ];
   }
-  
+
   toggle() {
     this.hideRooms = !this.hideRooms; //toggle the value of  hideRooms variable
   }
 
-  selectRoom(room:RoomList){
+  selectRoom(room: RoomList) {
     this.selectedRoom = room;
   }
 
+
+  addRoom() {
+    const room: RoomList = {
+      roomNumber: 5,
+      roomType: 'Delux Room',
+      amenities: 'Air Conditioner, Free Wi-Fi, Tv, Bathroom, Kitchen',
+      price: 5000,
+      photos: '',
+      checkinTime: new Date('19-Nov-2021'),
+      checkoutTime: new Date('22-Nov-2021')
+    };
+    // this.roomList.push(room);
+
+    // to create a new instance
+    this.roomList = [...this.roomList, room];
+
+  }
 }
